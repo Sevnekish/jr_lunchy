3bfff8e8b500d81f517df633084cf95a13b3638c
Nikita Khmelevsky <sevnekish@gmail.com>
2015-10-29 22:06:41 +0300

    Day menu form complite

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,6 +18,8 @@ class Item < ActiveRecord::Base
   has_many :day_menu_items
   has_many :day_menus, through: :day_menu_items
 
+  # scope :by_category, -> { order(price: :desc) }
+
   # def for_this_day(date)
     
   # end

135eafbf2ef0baa32f72ab9090cbf8ea570f0ce4
Nikita Khmelevsky <sevnekish@gmail.com>
2015-11-08 22:03:02 +0300

    Order feature spec for user interface

--- spec/features/order_feature_spec.rb
+++ spec/features/order_feature_spec.rb
@@ -2,91 +2,83 @@ require 'rails_helper'
 
 RSpec.feature "Order", type: :feature do
 
-  # before (:each) do
-  #   @admin = FactoryGirl.create(:user, admin: true)
+  before (:each) do
+    @admin = FactoryGirl.create(:user, admin: true)
+    @user = FactoryGirl.create(:user)
 
-  #   @categories = FactoryGirl.create_list(:category, 3)
-  #   @menu_items = []
-  #   @categories.each do |category|
-  #     items = FactoryGirl.create_list(:item, 3, category: category)
-  #     @menu_items = @menu_items + items.sample(2)
-  #   end
+    @today_menu = FactoryGirl.create(:day_menu)
+  end
 
-  #   @not_menu_items = Item.all - @menu_items
+  feature "Add new" do
+    subject { page }
 
-  #   @monday_id = 'monday'.to_date.wday
-  # end
+    feature 'with logged in user' do
 
-  # feature "Add new" do
-  #   before (:each) do
-  #     login_as(@admin, :scope => :user)
-  #     visit "/admin/day_menus/new"
-  #   end
+      before (:each) do
+        login_as(@user, :scope => :user)
+        visit "/orders/new"
+      end
 
-  #   subject { page }
+      subject { page }
 
-  #   it { should have_selector "input[type=submit][value='Create Day menu']" }
-  #   Date::DAYNAMES.each do |day_name|
-  #     it { should have_content day_name }
-  #   end
+      it { should have_button 'Create Order' }
 
-  #   it "should have each category" do
-  #     Category.all do |category|
-  #       should have_content category.name
-  #     end
-  #   end
+      it "should have each category" do
+        Category.all do |category|
+          should have_content category.name
+        end
+      end
 
-  #   it "should have each item" do
-  #     Item.all.each do |item|
-  #       should have_content item.name
-  #     end
-  #   end
+      it "should have each item" do
+        Item.all.each do |item|
+          should have_content item.name
+        end
+      end
 
-  #   feature 'with logged in user' do
 
-  #     scenario 'with valid data' do
-  #       # choose day of the week
-  #       choose "day_menu_day_id_#{@monday_id}"
+      scenario 'with valid data' do
 
-  #       # choose items
-  #       @menu_items.each do |item|
-  #         check "day_menu_item_ids_#{item.id}"
-  #       end
+        # choose items
+        all('select').each do |select|
+          select.find(:xpath, 'option[2]').select_option
+        end
 
-  #       click_button 'Create Day menu'
+        click_button 'Create Order'
 
-  #       expect(current_path).to eql("/admin/day_menus/#{DayMenu.last.id}")
+        expect(current_path).to eql("/orders")
 
-  #       should have_content 'Day menu was successfully created.'
+        should have_content 'Order was successfully created.'
 
-  #       @menu_items.each do |item|
-  #         should have_css "td", text: item.name
-  #         # should have_content item.name
-  #       end
+        Order.last.items.each do |item|
+          should have_content item.name, count: 2
+        end
 
-  #       #TODO make with regexp to find uniq
-  #       # @not_menu_items.each do |item|
-  #       #   should_not have_css "td", text: item.name
-  #       #   # should_not have_content item.name
-  #       # end
+        should_not have_link 'Make order'
+      end
 
-  #       should have_link 'Edit Day Menu'
-  #       should have_link 'Delete Day Menu'
-  #     end
+      scenario 'with invalid data' do
+        # not choose items
 
-  #     scenario 'with invalid data' do
-  #       # not choose day of the week
-  #       # not choose items
+        click_button 'Create Order'
 
-  #       click_button 'Create Day menu'
+        expect(current_path).to eql("/orders")
 
-  #       expect(current_path).to eql("/admin/day_menus")
+        should have_content 'can\'t be blank'
+      end
 
-  #       should have_content 'can\'t be blank'
-  #     end
+    end
 
-  #   end
-  # end
+    feature 'with logged ont user' do
+      before (:each) do
+        visit 'orders/new'
+      end
+      
+      it { should_not have_link 'Week menu' }
+
+      it { expect(current_path).to eql('/users/sign_in') }
+      it { should have_content 'You need to sign in or sign up before continuing.' }
+    end
+  end
 
 end
 
